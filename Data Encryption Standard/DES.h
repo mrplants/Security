80c54b5315f9db38dbc3bffec6b99eb2d9634f2c
Sean T Fitzgerald <fitzgerald.sean.t@gmail.com>
2015-11-19 23:47:36 -0500

    Wrote prototype description for DES scheme

--- Data Encryption Standard/DES.h
+++ Data Encryption Standard/DES.h
@@ -1,2 +1,11 @@
 // Data Encryption Standard
 // Sean T Fitzgerald
+
+/*
+INPUT:
+ 1. plaintextInput: The input string to the algorithm. Max length if 8 bytes (64 bits)
+ 2. key: The encryption key for the DES scheme
+OUTPUT:
+ Ciphertext string resulting from encrypting argument #1 with DES using argument #2 as the key
+*/
+char[8] encryptDES(char[8] plaintextInput, char[8] key);
