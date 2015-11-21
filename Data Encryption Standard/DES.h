75a6744748e46a8d43e91fe71f1a5f4bcfa892d7
Sean T Fitzgerald <fitzgerald.sean.t@gmail.com>
2015-11-21 01:38:32 -0500

    added prototype for decryption function. Very similar to encryption function.

--- Data Encryption Standard/DES.h
+++ Data Encryption Standard/DES.h
@@ -9,9 +9,21 @@ Input:
 Output:
  Success of the algorithm (0 is unsuccessful, otherwise successful)
 Notes:
- Function will attempt to decrypt even if key parity is incorrect (Output will be 0, however)
+ Function will attempt to encrypt even if key parity is incorrect (Output will be 0, however)
 */
 int encryptDES(const char[8] inputData, char[8] outputData, const char[8] key);
+
+/*
+Input:
+ 1. inputData: The input char array.
+ 2. outputData: The output char array.
+ 3. key: The encryption key
+Output:
+ Success of the algorithm (0 is unsuccessful, otherwise successful)
+Notes:
+ Function will attempt to decrypt even if key parity is incorrect (Output will be 0, however)
+*/
+int decryptDES(const char[8] inputData, char[8] outputData, const char[8] key);
 
 /*
 Input:
