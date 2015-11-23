77592e6469122834fb631018a2ca0c256bd77873
Sean T Fitzgerald <fitzgerald.sean.t@gmail.com>
2015-11-22 23:42:39 -0500

    created the encrypt and decrypt functions. Implemented the high level DES algorithm.

--- Data Encryption Standard/DES.c
+++ Data Encryption Standard/DES.c
@@ -9,10 +9,32 @@ INTERFACE
 
 /*
 Input:
- 1. key: The encryption key
- 2. resultantKeys:the per round keys based on argument #1
+ 1. inputData: The input char array.
+ 2. outputData: The output char array.
+ 3. key: The encryption key
+ 4. flag for decryption (0 for encryption, 1 for decryption)
+Output:
+ Success of the algorithm (0 is unsuccessful, otherwise successful)
+Notes:
+ The only difference between encryption and decryption is the order of the keys. Use this function for both and use argument #4 to differentiate
 */
-void generatePerRoundKeys(const char[8] key, char[16][6] resultantKeys);
+int performDES(const char[8] inputData, char[8] outputData, const char[8] key, const int decrypt)
+
+/*
+Input:
+ 1. inputData: The input char array to the function
+ 2. outputData: The output char array to the function post-encryption rounds
+ 3. roundKeys: The keys with which to perform the DES encryption rounds
+*/
+void performEcryptionRounds(const char[8] inputData, char[8] outputData, const char[16][6] roundKeys);
+
+/*
+Input:
+ 1. key: The encryption key
+ 2. resultantKeys: The per round keys based on argument #1
+ 3. backward: Flag for generating decryption keys (0 for forward/encrypt, 1 for backward/decrypt)
+*/
+void generatePerRoundKeys(const char[8] key, char[16][6] resultantKeys, int backward);
 
 /*
 Input:
@@ -26,12 +48,12 @@ Input:
  2. roundResult: The char array result of the round
  3. roundKey: The key associated with this round
 */
-void generateNewRound(const char[8] inputData, char[8] roundResult, const char[6] ruondKey);
+void generateNewRound(const char[8] inputData, char[8] roundResult, const char[6] roundKey);
 
 /*
 Input:
  1. inputData: The input char array to the function
- 2. outputData: The output char array with teh left/right halves swapped.
+ 2. outputData: The output char array with the left/right halves swapped.
 */
  void swapHalves(const char[8] inputData, char[8] outputData);
 
@@ -53,7 +75,17 @@ void keyCopy(const char[8] sourceKey, char[8] destinationKey);
 IMPLEMENTATION
 */
 
-int encryptDES(const char[8] inputData, char[8] outputData, const char[8] key);
+int encryptDES(const char[8] inputData, char[8] outputData, const char[8] key)
+{
+	return performDES(inputData, outputData, key, 0);
+}
+
+int decryptDES(const char[8] inputData, char[8] outputData, const char[8] key)
+{
+	return performDES(inputData, outputData, key, 1);
+}
+
+int performDES(const char[8] inputData, char[8] outputData, const char[8] key, const int decrypt)
 {
 	/* Function temporary variables: */
 	int successValue = 1; // Bias toward success
@@ -65,22 +97,17 @@ int encryptDES(const char[8] inputData, char[8] outputData, const char[8] key);
 	{
 		successValue = 0; // 0 means unsuccessful algorithm. Still attempt encryption with non-parity key
 	}
+
+	keyCopy(inputData, tempData); // Initialize the tempData with the initial data
 	
 	// Perform the initial permutation
-	generateInitialPermutation(inputData, tempData);
+	generateInitialPermutation(tempData, outputData);
+	keyCopy(outputData, tempData);
 
-	// Create the keys for each round
-	generatePerRoundKeys(key, roundKeys);
-
-	// Initialize tempData with inputData before key rounds
-	keyCopy(inputData, tempData);
-
-	// Run the 16 rounds
-	for (int i = 0; i < 16; ++i)
-	{
-		generateNewRound(tempData, outputData, roundKeys[i]); // Run the round
-		keyCopy(outputData, tempData); // Save the result for the next round
-	}
+	// Create the keys for each round & run the rounds
+	generatePerRoundKeys(key, roundKeys, decrypt);
+	performEcryptionRounds(tempData, outputData, roundKeys);
+	keyCopy(outputData, tempData);
 
 	// Swap the halves of the data
 	swapHalves(tempData, outputData);
@@ -91,4 +118,19 @@ int encryptDES(const char[8] inputData, char[8] outputData, const char[8] key);
 
 	// Return the result
 	return successValue;
+}
+
+void performEcryptionRounds(const char[8] inputData, char[8] outputData, const char[16][6] roundKeys)
+{
+	char[8] tempData; // Temporary data buffer for in-between encryption steps
+
+	// Initialize tempData with inputData before key rounds
+	keyCopy(inputData, tempData);
+
+	// Run the 16 rounds
+	for (int i = 0; i < 16; ++i)
+	{
+		generateNewRound(tempData, outputData, roundKeys[i]); // Run the round
+		keyCopy(outputData, tempData); // Save the result for the next round
+	}
 }
