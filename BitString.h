0041f86e6a7e870f9aa9e8292ad1667522cf8842
Meaghan Fitzgerald <MeaghanFitzgerald@Meaghans-MacBook-Pro.local>
2015-12-27 16:34:39 -0500

    adding a data type for bit manipulation

---
+++ BitString.h
@@ -0,0 +1,7 @@
+// Bit String
+// Sean T Fitzgerald
+
+/*
+This header file describes a data abstraction type which represents a string of bits.
+This is necessary because I don't want to complicate other code bases with bit manipulation.
+*/
